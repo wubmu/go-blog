02df44d7f1a9ff3815e9b780b33c997f43b5fc53
wubmu <923185028@qq.com>
2022-04-22 23:57:50 +0800

    写了以下博客的需要的文件

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -15,18 +14,6 @@ type IndexData struct {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	//返回json数据
-	w.Header().Set("Content-Type", "application/json")
-
-	var indexData IndexData
-	indexData.Title = "Go博客"
-	indexData.Desc = "现在刚入门"
-
-	jsonStr, _ := json.Marshal(indexData)
-
-	w.Write(jsonStr)
-}
-
-func IndexHtml(w http.ResponseWriter, request *http.Request) {
 	var indexData IndexData
 	indexData.Title = "Go博客"
 	indexData.Desc = "页面模板使用"
@@ -34,7 +21,20 @@ func IndexHtml(w http.ResponseWriter, request *http.Request) {
 
 	// 1.拿到当前路径
 	path, _ := os.Getwd()
-	t, _ = t.ParseFiles(path + "/template/index.html")
+
+	//访问博客首页模板的时候，因为有多个模板的嵌套，解析文件的时候，需要将其所有设计到的模板进行解析
+
+	home := path + "/template/home.html"
+	header := path + "/template/layout/home.html"
+	footer := path + "/template/layout/home.html"
+	personal := path + "/template/layout/home.html"
+	post := path + "/template/layout/home.html"
+	pagination := path + "/template/layout/home.html"
+
+	t, _ = t.ParseFiles(path+"/template/index.html", home, header, footer, personal, post, pagination)
+
+	// 页面上设计到的所有数据，必须定义
+
 	t.Execute(w, indexData)
 }
 
@@ -47,7 +47,7 @@ func main() {
 
 	http.HandleFunc("/", index)
 
-	http.HandleFunc("/index.html", IndexHtml)
+	//http.HandleFunc("/index.html", IndexHtml)
 	if err := server.ListenAndServe(); err != nil {
 		log.Panicln(err)
 	}
---
+++ models/category.go
@@ -0,0 +1,9 @@
+package models
+
+// 从数据库取数据
+type Category struct {
+	Cid      int
+	Name     string
+	CreateAt string
+	UpdateAt string
+}
